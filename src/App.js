f8e502a3d51b86b926c31e64dc0f3e9402e081ee
prashant sikarwar <prashant97sikarwar@gmail.com>
2020-07-10 23:31:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <ImageLinkForm/>
+  
+          {/*<Logo />
+          <Imagination /> 
+          <FaceRecogntion /> */}
+      </div>
+    );
+  }
+}
+
+
+export default App;

482178a637d705f0e4969d8b2a151241026c23d6
prashant sikarwar <prashant97sikarwar@gmail.com>
2020-08-07 11:02:07 +0530

    deploy

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://lit-fortress-77247.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -31,7 +31,7 @@ class Signin extends React.Component {
         if (user.id) {
           this.props.loadUser(user)
           this.props.onRouteChange('home');
-        }
+        }   
       })
   }
 
